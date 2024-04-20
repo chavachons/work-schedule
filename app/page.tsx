bf81257f2b08d20b37d7ab0109cd1d2168599dec
chavachon <chavachons@gmail.com>
2024-04-20 17:57:04 +0700

    update app name and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import prisma from "@/lib/prisma";
 import Calendar from "./ui/Caledar";
-import { useEffect } from "react";
-import { getSchedule } from "@/lib/schedule";
 export default async function Home() {
-  // const schedule = await getSchedule();
-  // console.log(schedule);
   return (
     <main className="flex min-h-screen flex items-center justify-between">
       <Calendar />
